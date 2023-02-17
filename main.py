a06c3976f4f3e6058f38e12ba26463443efb45ff
eitanporat <eitan.porat@weizmann.ac.il>
2023-02-17 12:22:56 +0200

    revert back to original code from github repo

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import copy
 import json
 import os
 import warnings
-from config import FLAGS
+from absl import app, flags
+
 import torch
-# from tensorboardX import SummaryWriter
+from tensorboardX import SummaryWriter
 from torchvision.datasets import CIFAR10
 from torchvision.utils import make_grid, save_image
 from torchvision import transforms
@@ -13,6 +14,43 @@ from tqdm import trange
 from diffusion import GaussianDiffusionTrainer, GaussianDiffusionSampler
 from model import UNet
 from score.both import get_inception_and_fid_score
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_bool('train', False, help='train from scratch')
+flags.DEFINE_bool('eval', False, help='load ckpt.pt and evaluate FID and IS')
+# UNet
+flags.DEFINE_integer('ch', 128, help='base channel of UNet')
+flags.DEFINE_multi_integer('ch_mult', [1, 2, 2, 2], help='channel multiplier')
+flags.DEFINE_multi_integer('attn', [1], help='add attention to these levels')
+flags.DEFINE_integer('num_res_blocks', 2, help='# resblock in each level')
+flags.DEFINE_float('dropout', 0.1, help='dropout rate of resblock')
+# Gaussian Diffusion
+flags.DEFINE_float('beta_1', 1e-4, help='start beta value')
+flags.DEFINE_float('beta_T', 0.02, help='end beta value')
+flags.DEFINE_integer('T', 1000, help='total diffusion steps')
+flags.DEFINE_enum('mean_type', 'epsilon', ['xprev', 'xstart', 'epsilon'], help='predict variable')
+flags.DEFINE_enum('var_type', 'fixedlarge', ['fixedlarge', 'fixedsmall'], help='variance type')
+# Training
+flags.DEFINE_float('lr', 2e-4, help='target learning rate')
+flags.DEFINE_float('grad_clip', 1., help="gradient norm clipping")
+flags.DEFINE_integer('total_steps', 800000, help='total training steps')
+flags.DEFINE_integer('img_size', 32, help='image size')
+flags.DEFINE_integer('warmup', 5000, help='learning rate warmup')
+flags.DEFINE_integer('batch_size', 128, help='batch size')
+flags.DEFINE_integer('num_workers', 4, help='workers of Dataloader')
+flags.DEFINE_float('ema_decay', 0.9999, help="ema decay rate")
+flags.DEFINE_bool('parallel', False, help='multi gpu training')
+# Logging & Sampling
+flags.DEFINE_string('logdir', './logs/DDPM_CIFAR10_EPS', help='log directory')
+flags.DEFINE_integer('sample_size', 64, "sampling size of images")
+flags.DEFINE_integer('sample_step', 1000, help='frequency of sampling')
+# Evaluation
+flags.DEFINE_integer('save_step', 5000, help='frequency of saving checkpoints, 0 to disable during training')
+flags.DEFINE_integer('eval_step', 0, help='frequency of evaluating model, 0 to disable during training')
+flags.DEFINE_integer('num_images', 50000, help='the number of generated images for evaluation')
+flags.DEFINE_bool('fid_use_torch', False, help='calculate IS and FID on gpu')
+flags.DEFINE_string('fid_cache', './stats/cifar10.train.npz', help='FID cache')
 
 device = torch.device('cuda:0')
 
@@ -44,7 +82,7 @@ def evaluate(sampler, model):
         for i in trange(0, FLAGS.num_images, FLAGS.batch_size, desc=desc):
             batch_size = min(FLAGS.batch_size, FLAGS.num_images - i)
             x_T = torch.randn((batch_size, 3, FLAGS.img_size, FLAGS.img_size))
-            batch_images = sampler(model, x_T.to(device)).cpu()
+            batch_images = sampler(x_T.to(device)).cpu()
             images.append((batch_images + 1) / 2)
         images = torch.cat(images, dim=0).numpy()
     model.train()
@@ -72,7 +110,13 @@ def train():
     net_model = UNet(
         T=FLAGS.T, ch=FLAGS.ch, ch_mult=FLAGS.ch_mult, attn=FLAGS.attn,
         num_res_blocks=FLAGS.num_res_blocks, dropout=FLAGS.dropout)
+    
+    ckpt = torch.load(os.path.join(FLAGS.logdir, 'ckpt.pt'))
+    net_model.load_state_dict(ckpt['net_model'])
+
     ema_model = copy.deepcopy(net_model)
+    ema_model.load_state_dict(ckpt['ema_model'])
+
     optim = torch.optim.Adam(net_model.parameters(), lr=FLAGS.lr)
     sched = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=warmup_lr)
     trainer = GaussianDiffusionTrainer(
@@ -89,7 +133,8 @@ def train():
         ema_sampler = torch.nn.DataParallel(ema_sampler)
 
     # log setup
-    os.makedirs(os.path.join(FLAGS.logdir, 'sample'))
+    if not os.path.exists(os.path.join(FLAGS.logdir, 'sample')):
+        os.makedirs(os.path.join(FLAGS.logdir, 'sample'))
     x_T = torch.randn(FLAGS.sample_size, 3, FLAGS.img_size, FLAGS.img_size)
     x_T = x_T.to(device)
     grid = (make_grid(next(iter(dataloader))[0][:FLAGS.sample_size]) + 1) / 2
